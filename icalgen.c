0f485b3ec2abd21945e65ec79d31836807fdabb1
Derek Redfern <redfern.derek@gmail.com>
2012-10-24 03:48:09 -0400

    Supports recurring events

--- icalgen.c
+++ icalgen.c
@@ -4,10 +4,7 @@
 #include <string.h>
 #include <time.h>
 
-//TODO: make sure that filename ends in .ics
-//      implement organizer name and email?
-
-//Credit: maerics on stackoverflow for date to day algorithm
+//TODO: implement organizer name and email?
 
 void printIntro(){
     puts("iCal Generator for Linux");
@@ -84,6 +81,52 @@ void printCurrentTime(FILE *file){
     fprintf(file,"LAST-MODIFIED:%i%i%iT%s%s%sZ\n",year,month,day,hrstr,minstr,secstr);
 }
 
+char *dateToDay(char *datestr, char *timestr) {
+    //takes a date string of format YYYYmmdd and a time string of format HHMMSS
+    
+    struct tm tm;
+    char *temp=(char*)malloc(sizeof(char)*5);
+    strncpy(temp,datestr,4);
+    temp[4]='\0';
+    tm.tm_year=atoi(temp)-1900;
+    strncpy(temp,datestr+4,2);
+    temp[2]='\0';
+    tm.tm_mon=atoi(temp)-1;
+    strncpy(temp,datestr+6,2);
+    temp[2]='\0';
+    tm.tm_mday=atoi(temp);
+    strncpy(temp,timestr,2);
+    temp[2]='\0';
+    tm.tm_hour=atoi(temp);
+    strncpy(temp,timestr+2,2);
+    temp[2]='\0';
+    tm.tm_min=atoi(temp);
+    strncpy(temp,timestr+4,2);
+    temp[2]='\0';
+    tm.tm_sec=atoi(temp);
+    tm.tm_isdst=-1;
+    int day=-1;
+
+    time_t t = mktime(&tm);
+    day = localtime(&t)->tm_wday; // Sunday=0, Monday=1, etc.
+    if (day==0) {
+    return("SU");
+    } else if (day==1) {
+    return("MO");
+    } else if (day==2) {
+    return("TU");
+    } else if (day==3) {
+    return("WE");
+    } else if (day==4) {
+    return("TH");
+    } else if (day==5) {
+    return("FR");
+    } else if (day==6) {
+    return("SA");
+    }
+    return("");
+}
+
 int main(int argc,char*argv[]){
     system("clear");
     printIntro();
@@ -101,7 +144,7 @@ PRIORITY:5\nTRANSP:OPAQUE\n");
     fprintf(file,"UID:thisisarandomuid%i\n",rand());
     printCurrentTime(file);
 
-    puts("Event title:");
+    puts("\nEvent title:");
     char title[201];
     fgets(title,201,stdin);
     puts("\nEvent description:");
@@ -120,14 +163,14 @@ DESCRIPTION:%sSUMMARY;LANGUAGE=en-us:%sLOCATION:%s",description,title,location);
     if (strstr(startdatestr,"\n")==NULL) {
         while ((flush=getchar()) != '\n' && flush != EOF); //flush stdin
     }
-    int startdate = atoi(startdatestr);
+    //int startdate = atoi(startdatestr);
     puts("\nStart time (hhmmss):");
     char starttimestr[7];
     fgets(starttimestr,7,stdin);
     if (strstr(starttimestr,"\n")==NULL) {
         while ((flush=getchar()) != '\n' && flush != EOF); //flush stdin
     }
-    int starttime = atoi(starttimestr);
+    //int starttime = atoi(starttimestr);
 
     puts("\nEnd date (yyyymmdd):");
     char enddatestr[9];
@@ -135,17 +178,131 @@ DESCRIPTION:%sSUMMARY;LANGUAGE=en-us:%sLOCATION:%s",description,title,location);
     if (strstr(enddatestr,"\n")==NULL) {
         while ((flush=getchar()) != '\n' && flush != EOF); //flush stdin
     }
-    int enddate = atoi(enddatestr);
+    //int enddate = atoi(enddatestr);
     puts("\nEnd time (hhmmss):");
     char endtimestr[7];
     fgets(endtimestr,7,stdin);
     if (strstr(endtimestr,"\n")==NULL) {
         while ((flush=getchar()) != '\n' && flush != EOF); //flush stdin
     }
-    int endtime = atoi(endtimestr);
+    //int endtime = atoi(endtimestr);
 
     fprintf(file,"DTEND;TZID=\"Eastern Standard Time\":%sT%s\n",enddatestr,endtimestr);
-    fprintf(file,"DTSTART;TZID=\"Eastern Standard Time\":%sT%s\n",startdatestr,starttimestr);    
+    fprintf(file,"DTSTART;TZID=\"Eastern Standard Time\":%sT%s\n",startdatestr,starttimestr);
+    
+    puts("\nDo you want this event to recur? [y/n]");
+    char confirm='a';
+    while(!((confirm=='y')||(confirm=='n'))){ //wait for valid input
+        confirm=getchar(); //get first character from stdin
+        while ((flush=getchar()) != '\n' && flush != EOF); //flush stdin
+    }
+    if(confirm=='y'){
+        puts("\nHow often do you want this event to recur?");
+        puts("Daily/weekly/monthly/yearly? [d/w/m/y]");
+        confirm='a';
+        while(!((confirm=='d')||(confirm=='w')||(confirm=='m')||(confirm=='y'))){ //wait for valid input
+            confirm=getchar(); //get first character from stdin
+            while ((flush=getchar()) != '\n' && flush != EOF); //flush stdin
+        }
+        char *freqstr;
+        if (confirm=='d'){
+            freqstr="day";
+            fprintf(file, "RRULE:FREQ=DAILY;");
+        } else if (confirm=='w') {
+            freqstr="week";
+            fprintf(file, "RRULE:FREQ=WEEKLY;");
+            puts("\nEnter days of the week to repeat on.");
+            puts("0=Su,1=M,2=Tu,3=W,4=Th,5=F,6=Sa");
+            puts("Example: 12345 to repeat on every weekday.");
+            char weekday[8];
+            fgets(weekday,8,stdin);
+            if (strstr(weekday,"\n")==NULL) {
+                while ((flush=getchar()) != '\n' && flush != EOF); //flush stdin
+            }
+            int weekdaybool[7] = {0,0,0,0,0,0,0};
+            char weekdaynames[15]="";
+            int i;
+            for (i=0;i<strlen(weekday);i++){
+                if(weekday[i]=='0'&&!weekdaybool[0]){
+                    strcat(weekdaynames,"SU");
+                    weekdaybool[0]=1;
+                } else if(weekday[i]=='1'&&!weekdaybool[1]){
+                    strcat(weekdaynames,"MO");
+                    weekdaybool[1]=1;
+                } else if(weekday[i]=='2'&&!weekdaybool[2]){
+                    strcat(weekdaynames,"TU");
+                    weekdaybool[2]=1;
+                } else if(weekday[i]=='3'&&!weekdaybool[3]){
+                    strcat(weekdaynames,"WE");
+                    weekdaybool[3]=1;
+                } else if(weekday[i]=='4'&&!weekdaybool[4]){
+                    strcat(weekdaynames,"TH");
+                    weekdaybool[4]=1;
+                } else if(weekday[i]=='5'&&!weekdaybool[5]){
+                    strcat(weekdaynames,"FR");
+                    weekdaybool[5]=1;
+                } else if(weekday[i]=='6'&&!weekdaybool[6]){
+                    strcat(weekdaynames,"SA");
+                    weekdaybool[6]=1;
+                }
+            }
+        } else if (confirm=='m') {
+            freqstr="month";
+            fprintf(file, "RRULE:FREQ=MONTHLY;");
+            puts("\nRecur by day (M-Su) or by date (1-31)? [day/date]");
+            char daydate[5]="";
+            while (strcmp(daydate,"day")&&strcmp(daydate,"date")) {
+                fgets(daydate,5,stdin);
+                if (strstr(daydate,"\n")==NULL) {
+                    while ((flush=getchar()) != '\n' && flush != EOF); //flush stdin
+                }
+                daydate[strlen(daydate)-1]='\0';
+            }
+            if (!strcmp(daydate,"day")){
+                int weeknum = ((startdatestr[6]-48)*10+(startdatestr[7]-48))/7+1;
+                char *weekday=dateToDay(startdatestr,starttimestr);
+                fprintf(file,"BYDAY=%i%s\n",weeknum,weekday);
+            } else {
+                fprintf(file,"BYMONTHDAY:%c%c",startdatestr[6],startdatestr[7]);
+            }
+        } else if (confirm=='y') {
+            freqstr="year";
+            fprintf(file, "RRULE:FREQ=YEARLY;BYMONTH=%c%c;",startdatestr[4],startdatestr[5]);
+            puts("\nRecur by day (M-Su) or by date (1-31)? [day/date]");
+            char daydate[5]="";
+            while (strcmp(daydate,"day")&&strcmp(daydate,"date")) {
+                fgets(daydate,5,stdin);
+                if (strstr(daydate,"\n")==NULL) {
+                    while ((flush=getchar()) != '\n' && flush != EOF); //flush stdin
+                }
+                daydate[strlen(daydate)-1]='\0';
+            }
+            if (!strcmp(daydate,"day")){
+                int weeknum = ((startdatestr[6]-48)*10+(startdatestr[7]-48))/7+1;
+                char *weekday=dateToDay(startdatestr,starttimestr);
+                fprintf(file,"BYDAY=%i%s\n",weeknum,weekday);
+            } else {
+                fprintf(file,"BYMONTHDAY:%c%c",startdatestr[6],startdatestr[7]);
+            }
+        } else {
+            puts("Something went wrong. Exiting.");
+            exit(-1);
+        }
+        printf("\nRecur at what frequency? (1 for every %s, 2 for every other %s, etc)\n",freqstr,freqstr);
+        char intervalstr[3];
+        fgets(intervalstr,3,stdin);
+        if (strstr(intervalstr,"\n")==NULL) {
+            while ((flush=getchar()) != '\n' && flush != EOF); //flush stdin
+        }
+        int interval = atoi(intervalstr);
+
+        puts("\nHow many recurrences do you want (NOT including the original)?");
+        char numrecurstr[5];
+        int numrecur;
+        fgets(numrecurstr,5,stdin);
+        numrecur=atoi(numrecurstr)+1; //TODO: check num of recurrences!
+        fprintf(file,"INTERVAL=%i;COUNT=%i\n",interval,numrecur);
+    }    
 
     fprintf(file,"END:VEVENT\nEND:VCALENDAR\n");
 
